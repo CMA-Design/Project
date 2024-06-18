bacceadd7b96786cb64f67389e0d6c2c84692443
Shashank Kumar <skumar18nams@gmail.com>
2024-06-18 14:53:27 +0530

    header updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import '../../fonts/Life is goofy.ttf';
-import '../../fonts/OstrichSans-Heavy.otf'; 
+import '../../fonts/OstrichSans-Heavy.otf';
 import './Header.css';
+
 function Header() {
   return (
     <header className="header">
       <div className="logo">
         <img src="logo.png" alt="Cheerful Mind Academy Logo" />
         <h1
-        style={{
+          style={{
             color: 'var(--accent-main, #E63747)',
             textAlign: 'left',
             fontFamily: 'Life is Goofy',
@@ -18,15 +19,15 @@ function Header() {
             lineHeight: 'normal',
             letterSpacing: '-1.106px'
           }}
-          > Cheerful Mind Academy</h1>
+        > Cheerful Mind Academy</h1>
       </div>
       <nav className="nav">
         <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/our-programs">Our Programs</a></li>
-          <li><a href="/after-school-programs">After School Programs</a></li>
-          <li><a href="/testimonials">Testimonials</a></li>
-          <li><a href="/gallery">Gallery</a></li>
+          <li><a href="/" style={{ fontFamily: 'Public Sans', fontSize: 18 }}>Home</a></li>
+          <li><a href="/our-programs" style={{ fontFamily: 'Public Sans', fontSize: 18 }}>Our Programs</a></li>
+          <li><a href="/after-school-programs" style={{ fontFamily: 'Public Sans', fontSize: 18 }}>After School Programs</a></li>
+          <li><a href="/testimonials" style={{ fontFamily: 'Public Sans', fontSize: 18 }}>Testimonials</a></li>
+          <li><a href="/gallery" style={{ fontFamily: 'Public Sans', fontSize: 18 }}>Gallery</a></li>
         </ul>
       </nav>
     </header>
