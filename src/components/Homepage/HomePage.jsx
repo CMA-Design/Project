a4aba06bf12e14584a9635e52ec141dd2bb750b6
Shashank Kumar <skumar18nams@gmail.com>
2024-06-30 22:28:56 +0530

    added features

--- src/components/Homepage/HomePage.jsx
+++ src/components/Homepage/HomePage.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
 import './HomePage.css';
 import '../../fonts/Life is goofy.ttf';
 import '../../fonts/OstrichSans-Heavy.otf';
-// import Header from '../Header/Header';
 
 function HomePage() {
   return (
     <div className="container">
       {/* <Header /> */}
       <section className="hero">
-        <div className="images">
+        <div className='image'>
           <img src="img1.png" alt="Image 1" className="image-top-left" />
           <img src="img2.png" alt="Image 2" className="image-top-right" />
         </div>
         <div className="banner">
-          <h1>CHEERFUL MIND ACADEMY</h1>
+          <h3>CHEERFUL MIND ACADEMY</h3>
         </div>
-        <div className="images">
+        <div className='image'>
           <img src="img3.png" alt="Image 3" className="image-bottom-left" />
           <img src="img4.png" alt="Image 4" className="image-bottom-right" />
         </div>
@@ -24,9 +24,15 @@ function HomePage() {
           <p>WELCOME TO CHEERFUL MIND ACADEMY</p>
           <p>A BEACON OF EARLY CHILDHOOD EDUCATION EXCELLENCE </p>
           <p>WITH A LEGACY FOR OVER 25 YEARS AND 10,000 YOUNG MINDS.</p>
+          {/* <img src="Arrow.png" alt="Arrow" className="arrow" /> */}
         </div>
       </section>
       <section className="welcome">
+        <div className='Border'>
+          <svg xmlns="http://www.w3.org/2000/svg" width="1347" height="1101" viewBox="0 0 1920 1101" fill="none">
+            <path d="M122.437 42.3259C97.0395 44.5734 28.8182 35.6524 -2.11774 30.9109L2.83718e-05 1100.05H1922.7L1920 26.0616C1925.36 25.363 1695.19 25.4328 1660.26 31.3013C1616.59 38.6369 1461.9 48.9407 1366.77 40.6396C1290.67 33.9987 1084.91 24.7994 991.545 21.0299C967.74 21.1361 911.204 23.8992 875.497 34.1012C839.79 44.3032 797.707 49.6167 781.128 50.9983C749.566 54.2927 671.967 54.3139 614.071 28.0438C556.174 1.77365 487.519 -0.630809 460.428 1.45073C448.029 4.50325 406.778 13.3237 325.031 19.7985C160.059 25.5948 154.465 36.707 122.437 42.3259Z" fill="#FFCA32"/>
+          </svg>
+        </div>
         <h2>WHERE PLAY IS PURPOSEFUL AND LEARNING IS PLAYFUL</h2>
         <p>Our philosophy revolves around the idea that children learn best through play. Play is not just a way for children to pass the time, it is their way of understanding the world, experimenting with ideas, and developing crucial skills. We embrace this philosophy wholeheartedly and weave it into every aspect of our curriculum and daily activities.</p>
       </section>
