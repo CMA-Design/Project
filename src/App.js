4ef97cec6fde740626e85f59672b3dca1af32f53
Shashank Kumar <skumar18nams@gmail.com>
2024-08-23 22:50:33 +0530

    restructure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/Homepage/HomePage';
-import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Playgroup from './components/OurPrograms/Playgroup';
 import { Route, Routes } from 'react-router-dom';
@@ -10,13 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<><Header/><HomePage/></>}/>
-          <Route path='/Playgroup' element={<Playgroup/>}/>
-        </Routes>
-        <Footer/>
-      </BrowserRouter>
+      <HomePage/>
     </div>
   );
 }
