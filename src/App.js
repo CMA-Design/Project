634367f6d8475daaaa0be0a82ce633be2cd4b2a2
Shashank Kumar <skumar18nams@gmail.com>
2024-09-30 14:49:07 +0530

    placed footer in homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/Homepage/HomePage';
-import Footer from './components/Footer/Footer';
 import Playgroup from './components/OurPrograms/Playgroup';
 import { Route, Routes } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
